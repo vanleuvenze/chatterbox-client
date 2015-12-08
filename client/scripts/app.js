2c6832e99e38469b6c430d51f8126d0c2d1921e7
ZACK DEAN <vanleuvenze@gmail.com>
2015-12-08 09:15:59 -0800

    Refactoring

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,3 @@
-$(document).ready(function () {
 
 //To Do//
 
@@ -6,64 +5,45 @@ $(document).ready(function () {
 
 //have messages auto refresh
 
-
-
 var app = {};
 
-var rawData;
-
 //raw data gatherd on initialization
+//this currentData variable will be updated on each fetch
+var currentData;
+var currentRoom = 'Homeroom';
 
-//initialize
-$.ajax({
-url: 'https://api.parse.com/1/classes/chatterbox',
-type: 'GET',
-contentType: 'application/json',
-success: function (data) {
-  //function that initializes the page
-  app.init(data);
-},
-error: function (data) {
-  // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-  console.error('chatterbox: Failed to send message');
-}
+var newRooms = {};  
+
+
+
+
+
+//modularize a bit more here
+app.fetch = function (callback) {
+  $.ajax({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    //take a callback and do something with the fetched data
+    if (callback) {
+      callback(data);
+    }
+
+    currentData = data.results;
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
 
 })
 
-
-
-
-//a
-app.init = function (data) {  
-  var rooms = _.uniq(_(data.results).map(function (message) {
-    return message.roomname;
-  }));
-  //add the rooms to the page
-  _.each(rooms, function (room) {
-    var element = $('<div class="room"></div>');
-    element.text(room);
-    element.attr('id', room);
-    $('.channels').append(element);
-  })
-
-  _(data.results).each(function (message) {
-    var element = $('<div class="message"></div>');
-    var userName = message.username;
-    element.text(userName + ":" + "    " + message.text);
-    $('.messages').append(element)
-  });
-  console.log(data);
-  rawData = data; 
 }
 
-
-
-
-
-app.send = function (message) {
+app.post = function (message) {
 
   $.ajax({
-  // This is the url you should use to communicate with the parse API server.
   url: 'https://api.parse.com/1/classes/chatterbox',
   type: 'POST',
   data: message,
@@ -72,79 +52,180 @@ app.send = function (message) {
     console.log('chatterbox: Message sent');
   },
   error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
     console.error('chatterbox: Failed to send message');
   }
   });
 
 }
+//initialize
+app.createPage = function () {
 
-app.addUser = function (username) {
+  app.fetch(app.init);
+
+}
+
+app.init = function (data) {  
+  var rooms = _.uniq(_(data.results).map(function (message) {
+    return message.roomname;
+  }));
+  var messages = data.results;
+  
+  //add the rooms to the page
+  _(rooms).each(function (room) {
+    $('.channels').append(app.createElement(room, true));
+  });
+
+  //add the messages to the page
+  _(messages).each(function (message) {
+    $('.messages').append(app.createElement(message, false))
+  });
+
+}
+
+app.createElement = function (rawMessage, bool) {
+  var newDiv = $('<div></div>');
+
+  if (bool === true) {
+    newDiv.attr('class', 'room');
+    newDiv.attr('id', rawMessage)
+    newDiv.text(rawMessage);
+
+  } else {
+    var usernameLink = $('<a></a>');
+    var userName = rawMessage.username;
+    var message = rawMessage.text;
+    usernameLink.text(userName)
+    newDiv.attr('class', 'message');
+
+    newDiv.text( userName + ":     " + message);
+  }
+
+  return newDiv;
+}
 
 
+//appends all the rooms and messages initially
+
+
+
+
+
+app.refresh = function (data) {
+
+  var room = currentRoom;
+  $('.messages').empty();
+
+  _.each(data, function (message) {
+    if (currentRoom === 'Homeroom') {
+      $('.messages').append(app.createElement(message, false));
+    }
+    else if (message.roomname === currentRoom) {
+      $('.messages').append(app.createElement(message, false));
+    }
+    
+  })
+  //use app get to get the data and re append it 
+
+}
+
+
+app.selectRoom = function (data) {
+  $('.messages').empty('');
+
+  _(currentData).each(function (message) {
+    if (message.roomname === currentRoom) {
+      $('.messages').prepend(app.createElement(message), false);
+    }
+  }); 
+}
+
+
+
+app.send = function (message) {
+
+  app.post(message);
+
+}
+app.createRoom = function (roomName) {
 
 
 
 }
 
 
-
-
-
-
-
-
+$(document).ready(function () {
 //send clickhandler
+  //add a roomname to add to
+  //add a username to add to
   $('.enter').on('click', function (data) {
-    console.log($(data.toElement).text());
-    var currentMessage = $('input').val(); 
-    $('input').val('');
+
+    var usernameOrMessage = $(data.toElement).text()
+    var entered = $('.send').val(); 
+    $('.send').val('');
+
+    if (usernameOrMessage === 'Enter username') {
+      //append current username
+      $('.currentUser').text(entered);
+      //change enter button to enter message
+      $('.enter').text('Send Message');
+
+    } else {
+    var currentUser = $('.currentUser').text();
 
     var message = {
-    username: 'Zacky',
-    text: currentMessage,
-    roomname: 'The Hovel'
+    username: currentUser,
+    text: entered,
+    roomname: currentRoom
     };
 
-    app.send(JSON.stringify(message));
+    app.send(JSON.stringify(message));  
+    }
 
   })
 
 //selecting a room clickhandler
+//need to make sure that we are only updating the individual room channels
+
   $('.channels').on('click', function (data) {
-    //when you click a room, select all messages based on that name
-    var selectedRoom = $(data.toElement).attr('id');
 
-    var appendMessage = $('<div class="message"></div>');
-    //remove divs from page to prep for new room exclusive divs
-    $('.messages').text('');
-
-    //iterate through data and if a message's roomname matches the roomname that was clicked, append it to the page.
-    _(rawData.results).each(function (message) {
-      if (message.roomname === selectedRoom) {
-        appendMessage.text(message.username + ':' + "    " + message.text)
-        $('.messages').append(appendMessage);
-      }
-    }) 
+    currentRoom = $(data.toElement).attr('id');
+    $('.selectedRoom').text(currentRoom);
+    app.selectRoom(data);
+    
     })
 
+  $('.newRoom').on('click', function (data) {
+    var newRoom = $('.enterRoom').val();
+    console.log(typeof newRoom)
+    $('.channels').prepend(app.createElement(newRoom, true));
+  })  
 
 
 
 
+app.createPage();
 
 
+setInterval(function () {
 
+  app.fetch();
+  app.refresh(currentData);
 
-
-
-
-
-
-
-
-
-
-
+}, 1000)
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
